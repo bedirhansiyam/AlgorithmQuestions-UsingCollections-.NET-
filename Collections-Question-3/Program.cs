a9ecbf5a03365f8e95b171371473d501cecca3cf
bedirhansiyam <118667545+bedirhansiyam@users.noreply.github.com>
2023-03-13 16:16:34 +0400

    Question 3 added

---
+++ Collections-Question-3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Collections_Question_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a sentence : ");
+        string sentence = Console.ReadLine();
+        char[] arrayChar = sentence.ToCharArray();
+        List<char> vowels = new List<char>();
+
+        foreach (var letter in arrayChar)
+        {
+            if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'|| letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+            {
+                vowels.Add(letter);
+            }
+        }
+        Console.WriteLine("*VOWELS IN SENTENCE*");
+        foreach (var item in vowels)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
